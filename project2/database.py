cba5909e2786d7022a1d3415d0e6e289967557d4
highcera <116418627+highcera@users.noreply.github.com>
2023-09-19 14:58:29 +0900

    O1

--- project2/database.py
+++ project2/database.py
@@ -1,32 +1,32 @@
 import pandas as pd
 
 def save(location, cleaness, built_in):
-    idx = len(pd.read_csv("C:/stockauto/flask/project2/database.csv"))
+    idx = len(pd.read_csv("P:/자동데이타생성/database1.csv"))
+    # E:/Repo_Office/Flask_Ex/project2/database1.csv
     new_df = pd.DataFrame({"location":location,
                            "cleaness":cleaness,
                            "built_in":built_in}, 
                          index = [idx])
-    new_df.to_csv("C:/stockauto/flask/project2/database.csv",mode = "a", header = False)
+    new_df.to_csv("P:/자동데이타생성/database1.csv",mode = "a", header = False)
     return None
 
 def load_list():
     house_list = []
-    df = pd.read_csv("C:/stockauto/flask/project2/database.csv")
+    df = pd.read_csv("P:/자동데이타생성/database1.csv")
     for i in range(len(df)):
         house_list.append(df.iloc[i].tolist())
     # print(house_list)
     return house_list
 
 def now_index():
-    df = pd.read_csv("C:/stockauto/flask/project2/database.csv")
+    df = pd.read_csv("P:/자동데이타생성/database1.csv")
     return len(df)-1
 
 
 def load_house(idx):
-    df = pd.read_csv("C:/stockauto/flask/project2/database.csv")
+    df = pd.read_csv("P:/자동데이타생성/database1.csv")
     house_info = df.iloc[idx]
     return house_info
-
 
 if __name__ =="__main__":
     load_list()
